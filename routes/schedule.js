f2c559ebe0f787b9a41ef1d65ad20c980eca4025
syauqeesy <syauqeesy@gmail.com>
2021-01-07 21:30:16 +0700

    Search flights done

--- routes/schedule.js
+++ routes/schedule.js
@@ -1,5 +1,7 @@
 const { Schedule, Facility } = require('../models')
+const { Op } = require('sequelize')
 const router = require('express').Router()
+const moment = require('moment')
 
 module.exports = router
   .post('/create', async (req, res) => {
@@ -19,7 +21,64 @@ module.exports = router
       })
     } catch (error) {
       console.log(error)
-      res.status(500).json({
+      return res.status(500).json({
+        status: 'Failed',
+        message: 'Internal server error'
+      })
+    }
+  })
+  .get('/', async (req, res) => {
+    try {
+      const index = parseInt(req.query.page) || 1
+      const numOfSchedules = await Schedule.findAll({
+        where: {
+          departureTime: {
+            [Op.between]: [moment(new Date()).format('YYYY-MM-DD hh:mm:ss'), '9999-12-31 00:00:00']
+          }
+        }
+      })
+      const schedules = await Schedule.findAll({
+        where: {
+          departureTime: {
+            [Op.between]: [moment(new Date()).format('YYYY-MM-DD hh:mm:ss'), '9999-12-31 00:00:00']
+          },
+          to: {
+            [Op.like]: `%${req.query.keyword || ''}%`
+          },
+          airline: {
+            [Op.like]: `%${req.query.airline || ''}%`
+          },
+          transit: {
+            [Op.like]: `%${req.query.transit || ''}%`
+          },
+        },
+        include: {
+          model: Facility,
+          as: 'facilities'
+        },
+        limit: 10,
+        offset: index * 10 - 10
+      })
+
+      if (schedules.length < 1) {
+        return res.status(404).json({
+          status: 'Failed',
+          message: 'Schedules not found'
+        })
+      }
+      return res.status(200).json({
+        status: 'Success',
+        message: 'Schedules data fetched',
+        schedules,
+        pagination: {
+          previousPage: index - 1 > 0 ? index - 1 : null,
+          currentPage: index,
+          nextPage: index + 1 <= Math.ceil(parseInt(numOfSchedules.length) / 10) ? index + 1 : null
+        }
+      })
+    } catch (error) {
+      console.log(error)
+      return res.status(500).json({
         status: 'Failed',
         message: 'Internal server error'
       })
